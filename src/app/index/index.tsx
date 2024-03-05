53d5d71c228c22772ca6d32e96f7dd8a3ce5e0bf
André Tavares <andretavares3@gmail.com>
2024-03-05 13:03:48 -0300

    creating layout and importing fonts

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,19 @@
 import { Text, View } from "react-native";
+import { styles } from './styles'
 
 export default function Index() {
     return (
-        <View>
-            <Text>React Native na Prática</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Escolha {"\n"}
+                <Text style={styles.subtitle}>os produtos</Text>
+            </Text>
+
+            <Text style={styles.message}>
+                Descubra receitas baseadas nos produtos que você escolheu
+            </Text>
+
+            
         </View>
     )
 }
