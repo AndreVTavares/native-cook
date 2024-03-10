97cd7164d9c75b0350c7fc879f4f8a3209ec4952
André Tavares <andretavares3@gmail.com>
2024-03-10 19:58:25 -0300

    creating selected managing for ingredient component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,20 @@
 import { ScrollView, Text, View } from "react-native"
 import { styles } from './styles'
 import { Ingredient } from "@/components/Ingredient"
+import { useState } from "react"
 
 export default function Index() {
+    const [selected, setSelected] = useState<string[]>([])
+
+    function handleToggleSelected(value: string) {
+        if (selected.includes(value)) {
+            return setSelected(state => state.filter((item) => item !== value))
+        }
+
+        setSelected(state => [...state, value])
+        console.log(selected)
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
@@ -14,10 +26,19 @@ export default function Index() {
                 Descubra receitas baseadas nos produtos que você escolheu
             </Text>
 
-            <ScrollView contentContainerStyle={styles.ingredientsContainer}>
+            <ScrollView
+                contentContainerStyle={styles.ingredientsContainer}
+                showsVerticalScrollIndicator={false}
+            >
                 {
-                    Array.from({ length:50 }).map((item, index) => (
-                        <Ingredient key={index} /> 
+                    Array.from({ length: 50 }).map((item, index) => (
+                        <Ingredient
+                            key={index}
+                            name="tomate"
+                            image=""
+                            selected={selected.includes(String(index))}
+                            onPress={() => handleToggleSelected(String(index))}
+                        />
                     ))
                 }
             </ScrollView>
